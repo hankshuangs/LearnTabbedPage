68fcda3e598de4165e2afec6a288e9f27ba0686f
hankshuangs <hankshuangs@gmail.com>
2020-08-26 02:19:25 +0800

    new

---
+++ LearnTabbedPage/LearnTabbedPage/LearnTabbedPagePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace LearnTabbedPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LearnTabbedPagePage : Xamarin.Forms.TabbedPage
+    {
+        public LearnTabbedPagePage()
+        {
+            InitializeComponent();
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom).SetBarItemColor(Color.Black).SetBarSelectedItemColor(Color.Blue);
+        }
+    }
+}
---
+++ LearnTabbedPage/LearnTabbedPage/TabbedPageWithDataTemplate.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LearnTabbedPage
+{
+	public partial class TabbedPageWithDataTemplate : TabbedPage
+	{
+		public TabbedPageWithDataTemplate()
+		{
+			InitializeComponent();
+
+
+			List<MyPage> pages = new List<MyPage>() {
+				new MyPage(){ DisplayName="I am page 1", DisplayTitle="首頁", IconName="home.png"},
+						new MyPage(){ DisplayName="I am page 2", DisplayTitle="新增會員", IconName="friend.png"},
+						//new MyPage(){ DisplayName="I am page 3", DisplayTitle="Page 3", IconName="ic_accessibillity.png"},
+						//new MyPage(){ DisplayName="I am page 4", DisplayTitle="Page 4", IconName="ic_accessibillity.png"},
+
+
+			};
+
+			ItemsSource = pages;
+
+		}
+	}
+
+
+
+
+
+	public class MyPage
+	{
+
+		public string DisplayName
+		{
+			get;
+			set;
+		}
+		public string IconName
+		{
+			get;
+			set;
+		}
+		public string DisplayTitle
+		{
+			get;
+			set;
+		}
+	}
+}
